5ff3653e821236b12d7ff98d7283139d1849f58c
TransformGit <2630243397@qq.com>
2017-11-26 10:24:54 +0800

    vue init

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import Router from 'vue-router'
+import App from './App'
+import routes from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.config.productionTip = false
+Vue.use(Router)
+Vue.use(ElementUI)
+const router = new Router({
+	routes
+})
+/* eslint-disable no-new */
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
